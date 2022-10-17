a4f32b700b270f518be200e0f12dcbc759120e60
StarGrim33 <prosky33@gmail.com>
2022-10-17 19:28:20 +0300

    1
    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HomeWorkFunction4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] map;
+        }
+
+        static char[,] ReadMap(string mapName)
+        {
+
+        }
+    }
+}
